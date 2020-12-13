b50b277aef421212d56887dea2fd118e85d313dd
JawadRaza8372 <jawadraza8372@gmail.com>
2020-12-13 14:05:43 +0500

    modification

--- src/About.jsx
+++ src/About.jsx
@@ -3,8 +3,20 @@ import Template from "./Template";
 function About(){
     return(<>
         <Template>
-            <div className="container px-4">
-<header><h1>Get Started</h1></header>
+            <div className="container px-2 py-2">
+            <br/>
+<header><h1>Getting Started</h1></header>
+<p>The aim of React Native Elements is to provide an all-in-one UI kit for creating apps in react native. There are many great ui components made by developers all around open source. React Native Elements takes the hassle of assembling these packages together by giving you a ready made kit with consistent api and look and feel.</p>
+<header><h1>Installation</h1></header>
+<p>Installing React Native Elements depends on your type of react native project.</p>
+<ul>
+    <li>Expo | Create React Native App</li>
+    <li>React Native CLI</li>
+</ul>
+
+         
+         
+         
             </div>
         </Template>
     </>);
