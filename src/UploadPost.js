fe694d1f948c6b2d49356029aa0be78f831ff7ce
JawadRaza8372 <jawadraza8372@gmail.com>
2021-03-23 12:29:34 +0500

    upload animations

--- src/UploadPost.js
+++ src/UploadPost.js
@@ -8,6 +8,7 @@ import { setRef } from '@material-ui/core';
 import {cate} from "./sideManuItems"
 import Template from './Template';
 import TemplateBlue from './TemplateBlue';
+import {motion} from "framer-motion"
 
 import FileUploaderModule from './FileUploaderModule';
 function UploadPost({rexx}) {
@@ -17,6 +18,9 @@ function UploadPost({rexx}) {
     let [filef,setfilf]=useState("");
     const [rex, setR] = useState(false)
     const [fimg,setfimg]=useState(null);
+    const ButtonCvarients2={init1:{y:'100vw'},anim1:{y:0,transition:{delay:3,duration:3}}};
+    const btnHover={hovers:{scale:1.1,textShadow:"0px 0px 8px rgb(255,255,255)",transition:{yoyo:Infinity,duration:0.4,type:"spring",stiffness:520}}}
+    
 let constt="";
     const handlein=(e)=>{
         const name=e.target.id;
@@ -92,24 +96,24 @@ console.log("success");      setfimg(url);setf("success")
                                               <form style={{ width:"40%",maxHeight:"80vh",overflowY:"auto",backgroundColor:"white",padding:"20px",margintop:"25px",borderRadius:"25px"}} onSubmit={submit}>
                                       <h2 className="blacksimpletxt" style={{color:"#2089dc",textAlign:"center"}}>Upload Component</h2><br/>
                                       <div className="form-group mb-3">
-                                          <input type="text" className="form-control" style={{border:"2px solid #2089dc",color:"#2089dc"}} onChange={handlein} id="title" autoComplete="off" placeholder="Title" required/>
+                                          <motion.input variants={btnHover} whileHover="hovers" type="text" className="form-control" style={{border:"2px solid #2089dc",color:"#2089dc"}} onChange={handlein} id="title" autoComplete="off" placeholder="Title" required/>
                                         </div>
                                         <div className="form-group mb-3">
-                                          <textarea type="text" className="form-control" style={{border:"2px solid #2089dc",color:"#2089dc"}} onChange={handlein} id="para" autoComplete="off" placeholder="Description"  required/>
+                                          <motion.textarea variants={btnHover} whileHover="hovers" type="text" className="form-control" style={{border:"2px solid #2089dc",color:"#2089dc"}} onChange={handlein} id="para" autoComplete="off" placeholder="Description"  required/>
                                         </div>
                                         <div className="form-group mb-3">
-                                          <textarea type="text" className="form-control" style={{border:"2px solid #2089dc",color:"#2089dc"}} onChange={handlein} id="code" autoComplete="off" placeholder="Code"/>
+                                          <motion.textarea variants={btnHover} whileHover="hovers" type="text" className="form-control" style={{border:"2px solid #2089dc",color:"#2089dc"}} onChange={handlein} id="code" autoComplete="off" placeholder="Code"/>
                                         </div>
                                         <div className="form-group mb-3">
-                                          <input  type="text" className="form-control" style={{border:"2px solid #2089dc",color:"#2089dc"}} onChange={handlein} id="video" autoComplete="off" placeholder="Video Link"/>
+                                          <motion.input variants={btnHover} whileHover="hovers"  type="text" className="form-control" style={{border:"2px solid #2089dc",color:"#2089dc"}} onChange={handlein} id="video" autoComplete="off" placeholder="Video Link"/>
                                         </div>
                                         <div className="row mb-3">
                                                                       <div className="col-12">
-                                                                       <select defaultValue="Category" onChange={handlein} style={{width:"100%",height:"40px",backgroundColor:"#f4f7fa",border:"2px solid #2089dc",color:"#2089dc"}} name="cars" id="category">
+                                                                       <motion.select variants={btnHover} whileHover="hovers" defaultValue="Category" onChange={handlein} style={{width:"100%",height:"40px",backgroundColor:"#f4f7fa",border:"2px solid #2089dc",color:"#2089dc"}} name="cars" id="category">
                                                                        <option  disabled>Category</option>
                                                                       {cate && cate.map((avin)=><option  className="maincolor" >{avin.text}</option>) }
                                                                        
-                                                                       </select></div></div><br/>
+                                                                       </motion.select></div></div><br/>
                                                                       
                                        
                                         <div className="form-group mb-3">
@@ -124,7 +128,7 @@ console.log("success");      setfimg(url);setf("success")
                                         </div>
                                                 
                                       
-                                        <button type="submit"  className="btn bttn btn-outline-primary">Upload Post</button>
+                                        <motion.button type="submit" variants={btnHover} whileHover="hovers"  className="btn bttn btn-outline-primary">Upload Post</motion.button>
                                       </form>
                                       <br/><br/>
                                       
