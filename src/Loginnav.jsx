fd79ee65a453f254e7dc68593c0d2a1d82b9f71f
Jawad Raza <jawadraza8372@gmail.com>
2021-05-31 17:01:33 +0500

    Update Loginnav.jsx

--- src/Loginnav.jsx
+++ src/Loginnav.jsx
@@ -4,13 +4,13 @@ import {auth} from "./FirebaseConfig"
 
 function Loginnav() {
     return (
-        <div style={{display:"flex",flexDirection:"row"}}>
+       <React.Fragment>
             <NavLink style={{marginRight:"15px"}} exact activeClassName="active_class"    className="nav-link h6"  to="/upload">Upload Component</NavLink>
 <NavLink style={{marginRight:"15px"}} exact activeClassName="active_class"    className="nav-link h6" to="/addCategory">Add Category</NavLink>
 <NavLink style={{marginRight:"15px"}} exact activeClassName="active_class"    className="nav-link h6" to="/ComponentMod">Update</NavLink>
         <NavLink style={{marginRight:"15px"}} exact activeClassName="active_class"    className="nav-link h6" onClick={()=>{auth.signOut()}}  to="">Logout</NavLink>
 
-        </div>
+        </React.Fragment>
     )
 }
 
