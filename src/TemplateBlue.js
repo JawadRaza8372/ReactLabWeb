4037b9571c40e486c9fb1a4a25b993f07a22dc97
Jawad Raza <jawadraza8372@gmail.com>
2021-07-20 12:45:49 +0500

    updates

--- src/TemplateBlue.js
+++ src/TemplateBlue.js
@@ -3,7 +3,7 @@ import React from 'react'
 function TemplateBlue({children}) {
     return (
         <div>
-           <section id="header" className="herobg d-flex overflow-hidden" style={{height:"90vh"}} >
+           <section id="header" className="herobg d-flex overflow-hidden" style={{height:"92vh"}} >
 <div className="container-fluid"  style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
     <div className="row">
 <div className="col-8 mx-auto">
